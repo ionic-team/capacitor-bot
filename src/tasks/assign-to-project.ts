4975855cded03b6e6591f443aed4b4b69a08cc63
Dan Imhoff <dwieeb@gmail.com>
2021-01-22 15:49:43 -0800

    Support different columns for PRs and draft PRs (#10)

--- src/tasks/assign-to-project.ts
+++ src/tasks/assign-to-project.ts
@@ -4,8 +4,14 @@ import * as github from '@actions/github';
 import type { GitHubClient } from '../client';
 import type { Task } from '../config';
 
+export interface AssignToProjectColumns {
+  readonly issue?: number;
+  readonly pr?: number;
+  readonly 'draft-pr'?: number;
+}
+
 export interface AssignToProjectConfig {
-  readonly 'column-id': number;
+  readonly columns?: AssignToProjectColumns;
 }
 
 export type AssignToProjectTask = Task<
@@ -15,20 +21,52 @@ export type AssignToProjectTask = Task<
 
 const run = async (
   client: GitHubClient,
-  { 'column-id': columnId }: AssignToProjectConfig,
+  { columns }: AssignToProjectConfig,
 ): Promise<void> => {
-  await client.projects.createCard({
-    column_id: columnId,
-    content_type:
-      github.context.eventName === 'pull_request' ? 'PullRequest' : 'Issue',
-    content_id:
-      github.context.payload.pull_request?.id ??
-      github.context.payload.issue?.id,
-  });
+  const columnId = getColumnId(columns);
 
-  core.info(
-    `added issue #${github.context.issue.number} to project column ${columnId}`,
-  );
+  if (!columnId) {
+    throw new Error('No column ID configured.');
+  }
+
+  try {
+    await client.projects.createCard({
+      column_id: columnId,
+      content_type:
+        github.context.eventName === 'pull_request' ? 'PullRequest' : 'Issue',
+      content_id:
+        github.context.payload.pull_request?.id ??
+        github.context.payload.issue?.id,
+    });
+
+    core.info(
+      `added issue #${github.context.issue.number} to project column ${columnId}`,
+    );
+  } catch (e) {
+    if (e.status === 422) {
+      core.warning(
+        `issue #${github.context.issue.number} is already in project`,
+      );
+    } else {
+      throw e;
+    }
+  }
+};
+
+const getColumnId = ({
+  issue,
+  pr,
+  'draft-pr': draftPR,
+}: AssignToProjectColumns = {}): number | undefined => {
+  if (github.context.eventName === 'pull_request') {
+    if (github.context.payload.pull_request?.draft) {
+      return draftPR;
+    } else {
+      return pr;
+    }
+  }
+
+  return issue;
 };
 
 export default run;
